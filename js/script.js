21ff99c8cdb3e12ab9d1f9535934b9638a95fff1
macsolom <cmhorton8@icloud.com>
2021-04-15 16:08:29 -0700

    Add minor tweaks to script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,31 @@
-function newItem(){
+function newItem() {
 
-  //  Add a new item to the list
+//  Add a new item to the list
   let li = $('<li></li>');
   let inputValue = $('#input').val();
-  li.append(inputValue);
+  li.append(document.createTextNode(inputValue));
 
   if (inputValue === '') {
-    alert("You must add an item!");
+    alert('You must add an item!')
   } else {
-    $('#list').append(li);
+    let list = $('#list');
+    list.append(li);
   }
 
-  // Cross out an item from the list
-  function crossOut() {
-    li.toggleClass('strike');
-  }
+// Cross out an item from the list
+  li.on('dblclick', function(){
+  li.toggleClass('strike');
+  });
 
-  li.on('dblclick', crossOut);
+// Add the delete "X" button & add class delete
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+li.append(crossOutButton);
+crossOutButton.append(document.createTextNode('X'));
+crossOutButton.on('click', function(){
+  li.addClass('delete');
+});
 
-  // Add the delete "X" button
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode('X'));
-  li.append(crossOutButton);
-
-  crossOutButton.on('click', deleteListItem);
-
-  // Add CLASS DELETE
-  function deleteListItem() {
-    li.addClass('delete');
-  }
-
-  // Ability to reorder the items:
-   $('#list').sortable();
+// Ability to reorder the items:
+  $('#list').sortable();
 
 }
