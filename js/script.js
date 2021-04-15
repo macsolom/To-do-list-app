b4fe0535b392949004a7410b03037d1e1f9fef8a
macsolom <cmhorton8@icloud.com>
2021-04-15 16:54:44 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,24 @@
-//  Add a new item to the list
-function newItem(){
-    let li = $("<li></li>");
-    let inputValue = $("#input").val();
-    li.append(inputValue);
+// Add a new item to the list
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-    if (inputValue === " ") {
-         alert("you must add an item!")
-    }
-    else{
-       $("#list").append(li);
-    }
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
+// Crossing an item out
+  function crossOut() {
+		li.toggleClass("strike");
+	}
 
-// Cross out an item from the list
-    li.on("dblclick",function crossOut(){
-          li.toggleClass("strike")
-      });
-
-// Add the delete "X" button & add class delete
-let crossOutButton = $("<crossOutButton></crossOutButton>");
-    crossOutButton.append(document.createTextNode("X"));
-    li.append(crossOutButton);
-
-
-    crossOutButton.on("click", deleteListItem);
-    function deleteListItem(){
-        li.addClass("delete")
-    }
-// Ability to reorder the items:
-    $('#list').sortable();
-}
+	li.on("dblclick", function crossOut() {
+		li.toggleClass("strike");
+	});
+// Adding a delete button
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+// Reorder list
+   $('#list').sortable();
